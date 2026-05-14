032760174d74783f7f2c6da91d172a1d6bea2cbf
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-05-14 13:03:00 -0600

    construir ruleta method addded

---
+++ CircularLinkedList.py
@@ -0,0 +1,28 @@
+import random
+from Node import Node
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tall = None
+        self.current = None
+        self.size = 0
+
+
+
+    def insertar_numero(self, number: int):
+        
+        new_node = Node(number)
+        if self.head is None:
+            self.head = new_node
+            self.tall = new_node
+            self.tall.next = self.head
+        
+
+        
+
+    def construir_ruleta(self):
+        for numero in range(37):
+            self.insertar_numero(numero)
+        self.current = self.head
+
